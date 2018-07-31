2e13e9f7be2cae026e54a711def83e8d362300da
seyive <sei4qurah@gmail.com>
2018-07-31 17:54:24 +0100

    updates

---
+++ myScript.js
@@ -0,0 +1,13 @@
+
+var animals = ['Sheep', 'Cat', 'Camel', 'Chicken', 'Chimpanzee']
+
+		let favouriteAnimal = prompt("Type in your favourite Animal");
+
+		alert("Gotcha!" + " " + favouriteAnimal + " " + "Is on our list");
+
+		if(animals.includes(favouriteAnimal));{
+
+		}else{
+
+			alert ("Ah ooooh!!!" + " " + favouriteAnimal + " " + "Favourite Animal not included.");
+		}
